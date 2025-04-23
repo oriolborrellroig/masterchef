c7c4ee3eb646d75420bd28dee262e6c4ad296fcd
Oriol Borrell Roig <oriolborrellroig@gmail.com>
2025-04-23 23:51:57 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("MasterChef II: Encrypted edition 🕵️‍♂️📜")
+st.title("MasterChef II: Encrypted edition🕵️‍♂️📜")
 
 opcions = st.selectbox("Tria una acció:", ["Encriptar", "Desencriptar"])
 clau = st.text_input("Clau")
