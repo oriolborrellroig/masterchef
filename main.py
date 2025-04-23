a60fea2f296a8d58d40530a63078eece128dd124
Oriol Borrell Roig <oriolborrellroig@gmail.com>
2025-04-23 23:42:57 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ import streamlit as st
 
 st.title("MasterChef II: Encrypted edition")
 
-input1 = st.text_input("Clau")
-input2 = st.text_area("Text")
+opcions = st.radio("Tria una acció:", ["Majúscules", "Minúscules", "Title Case"])
+clau = st.text_input("Clau")
+text = st.text_area("Text 2", height=400)  # quadre més alt
 
 if st.button("Executa"):
     resultat = (input1 + " " + input2).upper()  # Exemple simple
-    st.write("Resultat:", resultat)
+    st.write("Resultat:\n", resultat)
