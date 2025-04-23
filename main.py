d0848aa559168d49ee2b351ac0b0769815ce2700
Oriol Borrell Roig <oriolborrellroig@gmail.com>
2025-04-23 23:40:02 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ st.title("MasterChef II: Encrypted edition")
 input1 = st.text_input("Clau")
 input2 = st.text_area("Text")
 
-if st.button("Transforma"):
+if st.button("Executa"):
     resultat = (input1 + " " + input2).upper()  # Exemple simple
     st.write("Resultat:", resultat)
