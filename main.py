755bb57d62c0de5ba220db49216449848c9330db
Oriol Borrell Roig <oriolborrellroig@gmail.com>
2025-05-18 12:53:09 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,67 +1,12 @@
 import streamlit as st
-import pandas as pd
 
+st.title("MasterChef II: Encrypted edition🕵️‍♂️")
 
-users = {
-    "usuari1": "contrasenya123",
-    "maria": "1234"
-}
+opcions = st.selectbox("Tria una acció:", ["Encriptar", "Desencriptar"])
+clau = st.text_input("Clau")
+text = st.text_area("Text 2", height=400)  # quadre més alt
 
-    
-def load_text (user, password):
-    st.subheader("Text")
-
-    opcions = st.selectbox("Tria una acció:", ["Encriptar", "Desencriptar"])
-    clau = st.text_input("Clau")
-    text = st.text_area("Text 2", height=200)  # quadre més alt
-
-    if st.button("Executa"):
-        if user in users and users[user] == password:
-            resultat = (clau + " " + text).upper()  # Exemple simple
-            # st.write("Resultat:\n", resultat)
-            st.write("Resultat:\n", "Aquesta web encara no està disponible. Trenca't una mica el cap, va! 🧠" )
-
-        else:
-            st.error("Credencials incorrectes")
-
-
-def load_login ():
-    # Exemple simple
-
-    st.subheader("User")
-
-    col1, col2 = st.columns(2)
-
-    with col1:
-        user = st.text_input("Usuari")
-
-    with col2:
-        password = st.text_input("Contrasenya", type="password")
-    return user, password
-
-def load_sidebar():
-    data = pd.DataFrame({
-        "Usuari": ["AAAA", "BBBB", "CCCC"],
-        "Clau": ["AAAA", "BBBB", "CCCC"],
-        "Out Text": ["AAAAAAAAAAAAAAAAAAAA", "BBBBBBBBBBBBBBBBBBBB", "CCCCCCCCCCCCCCCCCCCC"]
-    })
-
-    data["Out Text"] = data["Out Text"].str[:7] + "..."
-    
-    st.table(data)
-
-
-def main():
-
-    with st.sidebar:
-        st.title("📖 Historial")
-        load_sidebar()
-    
-    user, password = load_login()
-    load_text(user, password)
-
-
-if __name__ == "__main__":
-    st.set_page_config(page_title="MasterChef II: Encrypted edition", page_icon="🕵️‍♂️")
-    st.title("MasterChef II: Encrypted edition")
-    main()
+if st.button("Executa"):
+    resultat = (clau + " " + text).upper()  # Exemple simple
+    # st.write("Resultat:\n", resultat)
+    st.write("Resultat:\n", "Aquesta web encara no està disponible. Trenca't una mica el cap, va" )
